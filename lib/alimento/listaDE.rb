57e6be4eca5867b8d5ff4d53d95552af0a0619e5
Javier Alberto <alu0100836400@ull.edu.es>
2017-11-10 12:21:29 +0000

    Pruebas de insertar pasadas

--- lib/alimento/listaDE.rb
+++ lib/alimento/listaDE.rb
@@ -11,7 +11,39 @@ module ListaDE
             @cola = nil
         end
         
-        
+        def insertar_cabeza(nuevoNodo)
+            if nuevoNodo.class == Nodo #Nodo
+                @aux = nuevoNodo
+            else
+                @aux = Nodo.new(nuevoNodo,nil,nil)
+            end
+            
+            if @cabeza != nil
+    			@cabeza.anterior = @aux
+    			@aux.siguiente = @cabeza
+    			@cabeza = @aux
+    		else
+    			@cabeza = @aux
+    			@cola = @aux
+            end
+    	end
+
+        def insertar_cola(nuevoNodo)
+            if nuevoNodo.class == Nodo #Nodo
+                @aux = nuevoNodo
+            else
+                @aux = Nodo.new(nuevoNodo,nil,nil)
+            end
+            
+            if @cola != nil
+    			@cola.siguiente = @aux
+    			@aux.anterior = @cola
+    			@cola = @aux
+    		else
+    			@cabeza = @aux
+    			@cola = @aux
+            end
+        end
 
     end
     
